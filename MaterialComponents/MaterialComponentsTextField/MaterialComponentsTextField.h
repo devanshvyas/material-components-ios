e8db5e0a49a211f9592c9cc03d4f389313a818b3
Devansh Vyas <devansh.vyas@solutionanalysts.com>
2019-07-26 15:55:46 +0530

    Material Textfield - Carthage support added.

---
+++ MaterialComponents/MaterialComponentsTextField/MaterialComponentsTextField.h
@@ -0,0 +1,19 @@
+//
+//  MaterialComponentsTextField.h
+//  MaterialComponentsTextField
+//
+//  Created by Devansh Vyas on 26/07/19.
+//  Copyright © 2019 Devansh Vyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialComponentsTextField.
+FOUNDATION_EXPORT double MaterialComponentsTextFieldVersionNumber;
+
+//! Project version string for MaterialComponentsTextField.
+FOUNDATION_EXPORT const unsigned char MaterialComponentsTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialComponentsTextField/PublicHeader.h>
+
+
